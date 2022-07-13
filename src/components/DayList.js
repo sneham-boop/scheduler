4205248fb2ea2812ee33f1076142d5daf4759ff3
Sneha Mahajan <sneh.km@gmail.com>
2022-07-13 15:11:35 -0400

    Add DayList to Application.js and render in app successfully with dummy data.

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,11 +1,18 @@
 import React from "react";
 import DayListItem from "./DayListItem";
 
-export default function DayList({ days, setDay, day }) {
+export default function DayList({ days, day, setDay }) {
   return (
     <ul>
       {days.map((d) => {
-        return <DayListItem key={d.id} selected={(d.name === day)} setDay={setDay} {...d} />;
+        return (
+          <DayListItem
+            key={d.id}
+            selected={d.name === day}
+            setDay={setDay}
+            {...d}
+          />
+        );
       })}
     </ul>
   );
