b3680915caf2b6ce4c0f405c599006866745ed24
Sneha Mahajan <sneh.km@gmail.com>
2022-07-15 15:42:30 -0400

    Refactor variable names for clarity in DayList and InterviewList components.

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import DayListItem from "./DayListItem";
 
-export default function DayList({ days, day, setDay }) {
+export default function DayList({ days, value, onChange }) {
   return (
     <ul>
       {days.map((d) => {
         return (
           <DayListItem
             key={d.id}
-            selected={d.name === day}
-            setDay={setDay}
+            selected={d.name === value}
+            setDay={onChange}
             {...d}
           />
         );
--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
 
-export default function InterviewerList({ interviewers, interviewer, setInterviewer}) {
+export default function InterviewerList({ interviewers, value, onChange}) {
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">
         {interviewers.map((i)=>{
-          return <InterviewerListItem key={i.id} setInterviewer={()=>setInterviewer(i.id)} selected={i.id === interviewer} {...i}/>
+          return <InterviewerListItem key={i.id} setInterviewer={()=>onChange(i.id)} selected={i.id === value} {...i}/>
         })}
       </ul>
     </section>
