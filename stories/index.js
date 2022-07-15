0eca38df6a0d83c31a861098319cdfd4d3f97e8e
Sneha Mahajan <sneh.km@gmail.com>
2022-07-15 17:16:42 -0400

    Create Empty component and add a story for it.

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 // Button
 storiesOf("Button", module)
@@ -143,5 +144,6 @@ storiesOf("Appointment", module)
     backgrounds: [{ name: "white", value: "#fff", default: true }],
   })
   .add("Appointment", () => <Appointment />)
-  .add("Appointment with time", () => <Appointment time="12pm"/>)
-  .add("Header", ()=> <Header time="12pm"/>);
+  .add("Appointment with time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
