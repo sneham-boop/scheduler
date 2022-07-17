26ec98a565d92b5c52cd3e587fabf2016ecc6bae
Sneha Mahajan <sneh.km@gmail.com>
2022-07-17 16:47:26 -0400

    Add state and props to form component.

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -2,39 +2,48 @@ import React, { useState } from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
-export default function Form({
-  student,
-  interviewers,
-  interviewer,
-  onSave,
-  onCancel,
-}) {
-  // const [student, setStudent] = useState("");
-  // const [interviewer, setInterviewer] = useState(0);
+export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setInterviewer(null);
+    setStudent("");
+  };
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
+  const save = () => {
+    props.onSave(student, interviewer);
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
             value={student}
+            onChange={(e) => setStudent(e.target.value)}
           />
         </form>
         <InterviewerList
-        interviewers={interviewers}
-        value={interviewer}
-        // onChange = {onSave}
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={onCancel}>
+          <Button danger onClick={cancel}>
             Cancel
           </Button>
-          <Button confirm onClick={onSave}>
+          <Button confirm onClick={save}>
             Save
           </Button>
         </section>
--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
 
-export default function InterviewerList({ interviewers, value, onChange}) {
+export default function InterviewerList({ interviewers, value, onChange }) {
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">
-        {interviewers.map((i)=>{
-          return <InterviewerListItem key={i.id} setInterviewer={()=>onChange(i.id)} selected={i.id === value} {...i}/>
+        {interviewers.map((interviewer) => {
+          return (
+            <InterviewerListItem
+              key={interviewer.id}
+              setInterviewer={() => onChange(interviewer.id)}
+              selected={interviewer.id === value}
+              {...interviewer}
+            />
+          );
         })}
       </ul>
     </section>
