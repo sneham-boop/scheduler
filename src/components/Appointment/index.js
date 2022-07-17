fd3ae00fb031b70f4a3e30193c59df5e6c0efe25
Sneha Mahajan <sneh.km@gmail.com>
2022-07-17 17:06:55 -0400

    Complete all components rendering in Storybook.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import 'components/Appointment/styles.scss';
-// import Empty from './Empty';
-// import Header from './Header';
-// import Show from './Show';
-
+import React from "react";
+import "components/Appointment/styles.scss";
+import Empty from "./Empty";
+import Header from "./Header";
+import Show from "./Show";
 
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      {props.time && <Header time={props.time} />}
+      {props.interview ? (
+        <Show  student={props.interview.student} interviewer={props.interview.interviewer}/>
+      ) : (
+        <Empty />
+      )}
+    </article>
   );
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -189,4 +189,14 @@ storiesOf("Appointment", module)
       onSave={action("onSave")}
       onCancel={action("onCancel")}
     />
+  ))
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
   ));
