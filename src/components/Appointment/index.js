75cd3ea671534431819b7e985b948385a64c6a2f
Sneha Mahajan <sneh.km@gmail.com>
2022-07-17 12:27:04 -0400

    Create Show, Confirm & Status components and their stories.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import 'components/Appointment/styles.scss';
+import Empty from './Empty';
+import Header from './Header';
+import Show from './Show';
+
 
 export default function Appointment(props) {
   return (
--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,9 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 // Button
 storiesOf("Button", module)
@@ -146,4 +149,20 @@ storiesOf("Appointment", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
-  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => (
+    <Show
+      student="Lynda Miller-Jones"
+      interviewer={interviewers[0]}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
+    />
+  ))
+  .add("Confirm", () => (
+    <Confirm
+      message="Delete the appointment?"
+      onConfirm={action("onConfirm")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Status", ()=> <Status message="Deleting"/>);
