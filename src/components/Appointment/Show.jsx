5c84fdee771038d1d030717ae802afa4b0d0b800
Sneha Mahajan <sneh.km@gmail.com>
2023-04-29 17:32:49 -0400

    Add new font

--- src/components/Appointment/Show.jsx
+++ src/components/Appointment/Show.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import styles from "./styles.module.scss";
+import { Noto_Sans } from "next/font/google";
+
+const noto = Noto_Sans({
+  weight: ["900"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
 
 export default function Show({ student, interviewer, onEdit, onDelete }) {
   // console.log("Inshow",interviewer);
@@ -10,7 +17,7 @@ export default function Show({ student, interviewer, onEdit, onDelete }) {
       <section className={styles["appointment__card-left"]}>
         <h2 className={styles["text--regular"]}>{student}</h2>
         <section className={styles["interviewer"]}>
-          <h4 className={styles["text--light"]}>Interviewer</h4>
+          <h4 className={`${noto.className} ${styles["text--light"]}`}>INTERVIEWER</h4>
           <h3 className={styles["text--regular"]}>{interviewer.name}</h3>
         </section>
       </section>
@@ -18,13 +25,13 @@ export default function Show({ student, interviewer, onEdit, onDelete }) {
         <section className={styles["appointment__actions"]}>
           <img
             className={styles["appointment__actions-button"]}
-            src="images/edit.png"
+            src="images/editButton.svg"
             alt="Edit"
             onClick={onEdit}
           />
           <img
             className={styles["appointment__actions-button"]}
-            src="images/trash.png"
+            src="images/deleteButton.svg"
             alt="Delete"
             onClick={onDelete}
           />
